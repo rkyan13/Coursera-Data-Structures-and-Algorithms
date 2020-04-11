2bda3024884d38edee8fb7a9729411990867bc18
kswamin4 <kswamin4@ford.com>
2020-04-10 23:34:09 -0400

    Alg9.1: week2 fibonacci sum last digit-instead of finding the n+2 fibonacci, find only last digit

--- C1-Algorithmic_Thinking/week2_algorithmic_warmup/wk2_6_fibonacci_sum_last_digit_ksw.cpp
+++ C1-Algorithmic_Thinking/week2_algorithmic_warmup/wk2_6_fibonacci_sum_last_digit_ksw.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -24,19 +25,21 @@ int fibonacci_sum_last_digit_naive(long long n) {
 
 
 
-long long int fibonacci_fast(int n) {
-    // write your code here
-    if (n <= 1)
-        return (long long)n;
+int get_fibonacci_last_digit_fast(long long n){
+  if(n<=1)
+     return n;
 
-    vector <long long int> fibVect(n+1);
-    fibVect[0] = 0;
-    fibVect[1] = 1;
-    for(int i=2; i<n+1; i++) {
-      fibVect[i]= fibVect[i-1]+fibVect[i-2];
-    }
+  int a_mod10 = 0;
+  int b_mod10 = 1;
+  int curr_mod10 ;
 
-    return fibVect[n];
+  for(long long i = 2; i < n+1; i++) {
+      curr_mod10 = (a_mod10 + b_mod10)%10;
+      a_mod10 = b_mod10;
+      b_mod10 = curr_mod10;
+  }
+
+  return curr_mod10;
 }
 
 
@@ -44,17 +47,27 @@ int fibonacci_sum_last_digit_fast(long long n) {
     if (n <= 1)
         return n;
 
-    long long fib_sum_n = fibonacci_fast(n+2) -1;
-    return fib_sum_n % 10;
+    int fib_Nplus2_lastDigit = get_fibonacci_last_digit_fast(n+2);
+    int fib_sum_N_lastDigit;
+    if(fib_Nplus2_lastDigit==0)
+       fib_sum_N_lastDigit = 9;
+    else
+       fib_sum_N_lastDigit = fib_Nplus2_lastDigit -1;
+
+    return fib_sum_N_lastDigit ;
 }
 
 
 void stress_test_solution() {
-    assert(fibonacci_sum_last_digit_fast(8) == 4);
-    assert(fibonacci_sum_last_digit_fast(2) == 2);
-    for (int n = 0; n < 20; ++n){
-        assert(fibonacci_sum_last_digit_naive(n) == fibonacci_sum_last_digit_fast(n));
-        cout<<"OK"<<endl;
+    assert(fibonacci_sum_last_digit_fast(8) == 4);//the sum of the Fibonacci numbers from F(0) to F(8) is 54 (last digit 4)
+    assert(fibonacci_sum_last_digit_fast(2) == 2);//the sum of the Fibonacci numbers from F(0) to F(2) is = 0+1+1 = 2 (last digit 2)
+    assert(fibonacci_sum_last_digit_fast(239) == 0);//the sum of the Fibonacci numbers from F(0) to F(239) , last digit is 0 says coursera .
+    for (int i = 0; i < 10; ++i){
+      //keeping rand at 20 because fibonacci_sum_last_digit_naive can handle only small numbers. Beyond that it will spew junk due to over flow
+      //but fibonacci_sum_last_digit_fast can handle even big numbers of the order long long
+      int n = rand()%20;
+      assert(fibonacci_sum_last_digit_naive(n) == fibonacci_sum_last_digit_fast(n));
+      cout<<"OK"<<endl;
     }
 }
 
@@ -63,6 +76,8 @@ int main() {
     long long n = 0;
     cin >> n;
     cout << fibonacci_sum_last_digit_fast(n) <<endl;
-    cout << fibonacci_sum_last_digit_naive(n)<<endl;
-    stress_test_solution();
+    //cout << fibonacci_sum_last_digit_naive(n)<<endl;
+    //stress_test_solution();
+    return 0;
 }
+//832564823476
