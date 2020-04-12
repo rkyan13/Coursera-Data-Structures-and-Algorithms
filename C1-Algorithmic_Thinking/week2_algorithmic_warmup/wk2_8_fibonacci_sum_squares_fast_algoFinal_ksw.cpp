541b8ef60ead56c8d2d057d4220e027f001a333b
kswamin4 <kswamin4@ford.com>
2020-04-12 01:27:23 -0400

    Alg11.2: wk2-pr-8, Algo1 and AlgoFinal; fixed the algorithms that were swapped . Added the algoFinal too

--- C1-Algorithmic_Thinking/week2_algorithmic_warmup/wk2_8_fibonacci_sum_squares_fast_algoFinal_ksw.cpp
+++ C1-Algorithmic_Thinking/week2_algorithmic_warmup/wk2_8_fibonacci_sum_squares_fast_algoFinal_ksw.cpp
@@ -9,6 +9,9 @@ The c++ index goes only from 0 to n-1.
 but Note that the fibonacci numbers are from idx 0 to n (n inclusive).
 so in the fibonacci for-loop/for-loops check until n+1
 */
+
+const int PISANO_OF_10 = 60;
+
 int fibonacci_sum_squares_last_digit_naive(long long n) {
     if (n <= 1)
         return n;
@@ -27,8 +30,14 @@ int fibonacci_sum_squares_last_digit_naive(long long n) {
     return sum % 10;
 }
 
-vector<long long int> fibonacci_fast_vector(int n) {
-    vector <long long int> fibResultVect(3);
+
+/*
+The c++ index goes only from 0 to n-1.
+but Note that the fibonacci numbers are from idx 0 to n (n inclusive).
+so in the for loop check until n+1
+*/
+vector<int> fibonacci_fast_last_digits_3vector(int n) {
+    vector <int> fibResultVect(3);
     // write your code here
     if (n == 0) {
       fibResultVect[0] = 0;
@@ -43,19 +52,21 @@ vector<long long int> fibonacci_fast_vector(int n) {
       return fibResultVect;
     }
 
-
-    vector <long long int> fibVect(n+1);
-    fibVect[0] = 0;
-    fibVect[1] = 1;
-    for(int i=2; i<n+1; i++) {
-      fibVect[i]= fibVect[i-1]+fibVect[i-2];
+    int a_mod10 = 0;
+    int b_mod10 = 1;
+    int curr_mod10 ;
+    curr_mod10 = (a_mod10 + b_mod10)%10;
+    for(int i = 3; i < n+1; i++) {
+        a_mod10    = b_mod10;
+        b_mod10    = curr_mod10;
+        curr_mod10 = (a_mod10 + b_mod10)%10;
     }
-
-    fibResultVect[0] = fibVect[n-2];
-    fibResultVect[1] = fibVect[n-1];
-    fibResultVect[2] = fibVect[n];
+    fibResultVect[0] = a_mod10;     //n-2
+    fibResultVect[1] = b_mod10;     //n-1
+    fibResultVect[2] = curr_mod10;  //n
 
     return fibResultVect;
+
 }
 
 
@@ -63,13 +74,15 @@ int fibonacci_sum_squares_last_digit_fast(long long n) {
     if (n <= 1)
         return n;
 
-    vector <long long int> fib_vect = fibonacci_fast_vector(n);
-    long long Fn_2 = fib_vect[0];
-    long long Fn_1 = fib_vect[1];
-    long long Fn   = fib_vect[2];
-    long long Fn_squareSum;
+    long long int effective_fibonacci_idx = n % PISANO_OF_10 ;
 
-    Fn_squareSum = (Fn_2 + Fn_1)*(Fn_1 + Fn);
+    vector <int> fib_vect = fibonacci_fast_last_digits_3vector(effective_fibonacci_idx);
+    int Fn_2_mod10 = fib_vect[0];
+    int Fn_1_mod10 = fib_vect[1];
+    int Fn_mod10   = fib_vect[2];
+    int Fn_squareSum;
+
+    Fn_squareSum = (Fn_2_mod10 + Fn_1_mod10)*(Fn_1_mod10 + Fn_mod10);
     return Fn_squareSum % 10;
 }
 
@@ -95,8 +108,8 @@ int main() {
     long long n = 0;
     cin >> n;
     cout << fibonacci_sum_squares_last_digit_fast(n) <<endl;
-    cout << fibonacci_sum_squares_last_digit_naive(n)<<endl;
-    stress_test_solution();
+    //cout << fibonacci_sum_squares_last_digit_naive(n)<<endl;
+    //stress_test_solution();
 
     return 0;
 }
